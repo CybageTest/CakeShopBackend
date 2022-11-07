f67d2f623a807f395d159cebc6a5acf3ee99c971
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 18:43:42 +0530

    Otp and email service implementaion in jwtService

--- src/main/java/com/littlejoys/service/JwtService.java
+++ src/main/java/com/littlejoys/service/JwtService.java
@@ -27,6 +27,12 @@ public class JwtService implements UserDetailsService {
 
 	@Autowired
 	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private OtpService otpService;
+	
+	@Autowired
+	private EmailService emailService;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -38,10 +44,10 @@ public class JwtService implements UserDetailsService {
 		final UserDetails userDetails = loadUserByUsername(username);
 		String newJwtToken = jwtUtil.generateToken(userDetails);
 		User user= userDao.findById(username).get();
-		//int otp= otpService.generateotp(username);
-		//String message = ("Your OTP for login is " + otp + " and will be valid for 5 minutes.");
-		//System.out.println("OTP JWT: "+message );
-		//emailService.sendEmail(user.getEmail(), "Otp for login", message);
+		int otp= otpService.generateotp(username);
+		String message = ("Your OTP for login is " + otp + " and will be valid for 5 minutes.");
+		System.out.println("OTP JWT: "+message );
+		emailService.sendEmail(user.getEmail(), "Otp for login", message);
 		return new JwtResponse(user, newJwtToken);
 	}
 
