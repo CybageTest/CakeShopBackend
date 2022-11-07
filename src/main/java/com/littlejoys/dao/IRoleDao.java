89e369fba81b67c82770e35731fa4d2e1b119ed1
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 17:34:25 +0530

    RoleDao created

---
+++ src/main/java/com/littlejoys/dao/IRoleDao.java
@@ -0,0 +1,11 @@
+package com.littlejoys.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.littlejoys.entity.Role;
+
+@Repository
+public interface IRoleDao extends JpaRepository<Role, String> {
+	
+}
