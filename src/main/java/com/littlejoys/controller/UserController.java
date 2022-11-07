a3467f9f259501fae59f72cce2ad9b54b6bc3315
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 18:18:49 +0530

    authenticate api created

--- src/main/java/com/littlejoys/controller/UserController.java
+++ src/main/java/com/littlejoys/controller/UserController.java
@@ -3,9 +3,14 @@ package com.littlejoys.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.littlejoys.entity.JwtRequest;
+import com.littlejoys.entity.JwtResponse;
+import com.littlejoys.service.JwtService;
 import com.littlejoys.service.UserService;
 
 @RestController
@@ -15,8 +20,18 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private JwtService jwtService;
+	
 	@PostConstruct
 	public void initRoleUsers() {
 		userService.initRolesAndUser();
 	}
+	
+	@PostMapping({ "/authenticate" })
+	public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+		return jwtService.createJwtToken(jwtRequest);
+	}
+	
+	
 }
