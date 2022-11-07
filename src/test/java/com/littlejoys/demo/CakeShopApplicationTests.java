4a8eb6c14311a112961e2e3ca47745e1bd612367
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 17:19:49 +0530

    Basic project structure created

---
+++ src/test/java/com/littlejoys/demo/CakeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlejoys.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
