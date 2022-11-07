c248db846125500ef826273d3fc5411427502835
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 18:10:50 +0530

    createJwtToken method created

--- src/main/java/com/littlejoys/service/JwtService.java
+++ src/main/java/com/littlejoys/service/JwtService.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +26,33 @@ public class JwtService implements UserDetailsService {
 	@Autowired
 	private JwtUtil jwtUtil;
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	public JwtResponse createJwtToken(JwtRequest jwtRequest) throws Exception {
+		String username = jwtRequest.getUsername();
+		String password = jwtRequest.getPassword();
+		authenticate(username, password);
+		final UserDetails userDetails = loadUserByUsername(username);
+		String newJwtToken = jwtUtil.generateToken(userDetails);
+		User user= userDao.findById(username).get();
+		int otp= otpService.generateotp(username);
+		String message = ("Your OTP for login is " + otp + " and will be valid for 5 minutes.");
+		System.out.println("OTP JWT: "+message );
+		//emailService.sendEmail(user.getEmail(), "Otp for login", message);
+		return new JwtResponse(user, newJwtToken);
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("User is disabled" + e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("BAD CREDENTIALS" + e);
+		}
+	}
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userDao.findById(username).get();
