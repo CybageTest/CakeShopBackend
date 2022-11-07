6e9f2ec94db3ce594cf5d68c1a29b1db3ad4b5be
Abhijeet <abhijeetgu@cybage.com>
2022-11-07 18:42:06 +0530

    OtpService created

---
+++ src/main/java/com/littlejoys/service/OtpService.java
@@ -0,0 +1,52 @@
+package com.littlejoys.service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+@Service
+public class OtpService {
+	public static final Integer EXPIRE_MINS=5;
+	private LoadingCache otpCache;
+	
+	public OtpService() {
+		super();
+		otpCache = CacheBuilder.newBuilder().expireAfterWrite(EXPIRE_MINS, TimeUnit.MINUTES).build(new CacheLoader() {
+			public Integer load(String key) {
+				return 0;
+			}
+
+			@Override
+			public Object load(Object key) throws Exception {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		});	
+	}
+		
+	public int generateotp(String key) {
+		Random random = new Random();
+		int otp = 100000 + random.nextInt(900000);
+		otpCache.put(key, otp);
+		return otp;
+	}
+
+	public int getOtp(String key) {
+		try {
+			return (int) otpCache.get(key);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+	public void clearOtp(String key) {
+		otpCache.invalidate(key);
+	}
+
+	
+}
