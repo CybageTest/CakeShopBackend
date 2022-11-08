a320d2b70244fc11815aababdf24179ca8ac409d
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 11:16:29 +0530

    findOfferById method created

--- src/main/java/com/littlejoys/service/OfferService.java
+++ src/main/java/com/littlejoys/service/OfferService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.littlejoys.dao.IOfferDao;
 import com.littlejoys.entity.Offer;
+import com.littlejoys.exception.ResourceNotFoundException;
 
 
 @Service
@@ -17,6 +18,10 @@ public class OfferService {
 	
 	public Offer addOffer(Offer offer) {
 		return offerDao.save(offer);
+	}
+	
+	public Offer findOfferById(long id) {
+		return offerDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("Offer(id) does not exist"));
 	}
 	
 	
