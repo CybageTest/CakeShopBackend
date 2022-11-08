bbee9b275a961443f846bb44f2b89cfb788bbdd7
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 12:07:18 +0530

    API created for editing cakes by Id

--- src/main/java/com/littlejoys/controller/CakeController.java
+++ src/main/java/com/littlejoys/controller/CakeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +65,11 @@ public class CakeController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Cake> deleteCakeById(@PathVariable long id) throws Exception{
 		return new ResponseEntity<Cake>(cakeService.deleteCakeById(id), HttpStatus.OK);
+	}
+	
+	@PutMapping("/editCake/{id}")
+	public ResponseEntity<Cake> editCakeById(@PathVariable long id, @RequestBody Cake cake) throws Exception{
+		return new ResponseEntity<Cake>(cakeService.editCakeById(id, cake), HttpStatus.OK);
 	}
 	
 
