fbeab4747daeaacda7d8d0eb77b136a58e964c4d
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 12:20:32 +0530

    API created for finding orders by category

--- src/main/java/com/littlejoys/controller/OrderController.java
+++ src/main/java/com/littlejoys/controller/OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.littlejoys.entity.CakeCategory;
 import com.littlejoys.entity.Order;
 import com.littlejoys.service.OrderService;
 
@@ -41,6 +42,11 @@ public class OrderController {
 	@DeleteMapping("/deleteOrder/{id}")
 	public ResponseEntity<Order> deleteOrderById(@PathVariable long id) throws Exception{
 		return new ResponseEntity<Order>(orderService.deleteOrderById(id), HttpStatus.OK);
+	}
+	
+	@GetMapping("/ordersByCategory/{category}")
+	public ResponseEntity<List<Order>> getOrdersByCakeCategory(@PathVariable CakeCategory category){
+		return new ResponseEntity<List<Order>>(orderService.findCakesByCategory(category), HttpStatus.FOUND);
 	}
 	
 }
