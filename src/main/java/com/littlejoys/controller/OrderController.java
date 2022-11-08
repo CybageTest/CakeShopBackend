bc42356f153557475a1764c10b29d528356977d3
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 12:19:40 +0530

    API created for finding orders By Id

--- src/main/java/com/littlejoys/controller/OrderController.java
+++ src/main/java/com/littlejoys/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,4 +32,9 @@ public class OrderController {
 		return new ResponseEntity<List<Order>>(orderService.getAllOrder(), HttpStatus.OK);
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Order> findOrderById(@PathVariable long id){
+		return new ResponseEntity<Order>(orderService.findOrderById(id), HttpStatus.FOUND);
+	}
+	
 }
