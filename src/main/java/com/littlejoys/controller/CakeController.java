4cd50ec620a69cd3334f9a5494dd3380f6c92eb1
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 12:06:47 +0530

    API created for deleting cakes by Id

--- src/main/java/com/littlejoys/controller/CakeController.java
+++ src/main/java/com/littlejoys/controller/CakeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,11 @@ public class CakeController {
 	@GetMapping("/byFlavours/{flavours}")
 	public ResponseEntity<List<Cake>> findAllCakesByFlavours(@PathVariable CakeFlavours flavours){
 		return new ResponseEntity<List<Cake>>(cakeService.findCakeByFlavours(flavours), HttpStatus.FOUND);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Cake> deleteCakeById(@PathVariable long id) throws Exception{
+		return new ResponseEntity<Cake>(cakeService.deleteCakeById(id), HttpStatus.OK);
 	}
 	
 
