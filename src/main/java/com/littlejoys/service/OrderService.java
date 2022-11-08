7a1d2d9c54fba31c61df27ffd3f227cbb3032835
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 11:43:07 +0530

    get all orders method created

--- src/main/java/com/littlejoys/service/OrderService.java
+++ src/main/java/com/littlejoys/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.littlejoys.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,4 +23,8 @@ public class OrderService {
 		return orderDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("Order(id) does not exist"));
 	}
 
+	public List<Order> getAllOrder() {
+		return orderDao.findAll();
+	}
+	
 }
