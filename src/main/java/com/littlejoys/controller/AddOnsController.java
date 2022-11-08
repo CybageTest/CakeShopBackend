24accf6b29033cb7302dcdd260bb5060b5f3e769
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 12:12:23 +0530

    API created for editing addons by Id

--- src/main/java/com/littlejoys/controller/AddOnsController.java
+++ src/main/java/com/littlejoys/controller/AddOnsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,11 @@ public class AddOnsController {
 	public ResponseEntity<AddOns> deleteAddOnsById(@PathVariable long id) throws Exception{
 		return new ResponseEntity<AddOns>(addOnsService.deleteAddOnById(id), HttpStatus.OK);
 	}
+	
+	@PutMapping("/editAddon/{id}")
+	public ResponseEntity<AddOns> editAddOnsById(@PathVariable long id, @RequestBody AddOns addons) throws Exception{
+		return new ResponseEntity<AddOns>(addOnsService.editAddon(id, addons), HttpStatus.OK);
+	}
+	
 
 }
