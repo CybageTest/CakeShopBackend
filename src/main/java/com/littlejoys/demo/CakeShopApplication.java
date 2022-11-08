b9da1952a4a494d9a266764ab68cf122186b8a8d
Abhijeet <abhijeetgu@cybage.com>
2022-11-08 11:47:06 +0530

    Annotation added in main method

--- src/main/java/com/littlejoys/demo/CakeShopApplication.java
+++ src/main/java/com/littlejoys/demo/CakeShopApplication.java
@@ -2,8 +2,12 @@ package com.littlejoys.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.littlejoys")
+@EnableJpaRepositories("com.littlejoys.dao")
+@EntityScan("com.littlejoys.entity")
 public class CakeShopApplication {
 
 	public static void main(String[] args) {
