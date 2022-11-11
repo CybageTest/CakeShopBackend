a55b9038c3e00332961f9c8da8d478c350985d5e
Abhijeet <abhijeetgu@cybage.com>
2022-11-11 15:42:11 +0530

    Lombok added for cake entity

--- src/main/java/com/littlejoys/entity/Cake.java
+++ src/main/java/com/littlejoys/entity/Cake.java
@@ -15,6 +15,13 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "cakes")
 public class Cake {
@@ -40,115 +47,5 @@ public class Cake {
 	@ManyToMany(mappedBy = "cakes", cascade = CascadeType.ALL)
 	@JsonIgnore
 	private Set<Order> order;
-
-	public Cake() {
-
-	}
-
-	public Cake(long id, String cakeName, double price, String description, int weight, CakeFlavours flavours,
-			CakeCategory category, CakeOccasions occasions) {
-		super();
-		this.id = id;
-		this.cakeName = cakeName;
-		this.price = price;
-		this.description = description;
-		this.weight = weight;
-		this.flavours = flavours;
-		this.category = category;
-		this.occasions = occasions;
-	}
-
-	public Cake(long id, String cakeName, double price, String description, int weight, CakeFlavours flavours,
-			CakeCategory category, CakeOccasions occasions, Set<Order> order) {
-		super();
-		this.id = id;
-		this.cakeName = cakeName;
-		this.price = price;
-		this.description = description;
-		this.weight = weight;
-		this.flavours = flavours;
-		this.category = category;
-		this.occasions = occasions;
-		this.order = order;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getCakeName() {
-		return cakeName;
-	}
-
-	public void setCakeName(String cakeName) {
-		this.cakeName = cakeName;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getWeight() {
-		return weight;
-	}
-
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-
-	public CakeFlavours getFlavours() {
-		return flavours;
-	}
-
-	public void setFlavours(CakeFlavours flavours) {
-		this.flavours = flavours;
-	}
-
-	public CakeCategory getCategory() {
-		return category;
-	}
-
-	public void setCategory(CakeCategory category) {
-		this.category = category;
-	}
-
-	public CakeOccasions getOccasions() {
-		return occasions;
-	}
-
-	public void setOccasions(CakeOccasions occasions) {
-		this.occasions = occasions;
-	}
-
-	public Set<Order> getOrder() {
-		return order;
-	}
-
-	public void setOrder(Set<Order> order) {
-		this.order = order;
-	}
-
-	@Override
-	public String toString() {
-		return "Cake [id=" + id + ", cakeName=" + cakeName + ", price=" + price + ", description=" + description
-				+ ", weight=" + weight + ", flavours=" + flavours + ", category=" + category + ", occasions="
-				+ occasions + ", order=" + order + "]";
-	}
 
 }
