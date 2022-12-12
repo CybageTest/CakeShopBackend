75678c7d11f85b90c0140e1f05eb764a9f555fe6
Abhijeet <abhijeetgu@cybage.com>
2022-12-12 10:44:35 +0530

    created bean for model mapper in main method

--- src/main/java/com/littlejoys/demo/CakeShopApplication.java
+++ src/main/java/com/littlejoys/demo/CakeShopApplication.java
@@ -1,8 +1,10 @@
 package com.littlejoys.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.littlejoys")
@@ -14,4 +16,9 @@ public class CakeShopApplication {
 		SpringApplication.run(CakeShopApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 }
