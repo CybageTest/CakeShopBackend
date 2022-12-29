eef7d45cdfe99969044fed6d09f1a2b26e4be25e
Abhijeet <abhijeetgu@cybage.com>
2022-12-29 15:52:36 +0530

    added method to get all flavours

--- src/main/java/com/littlejoys/service/CakeService.java
+++ src/main/java/com/littlejoys/service/CakeService.java
@@ -17,18 +17,21 @@ public class CakeService {
 
 	@Autowired
 	private ICakeDao cakeDao;
-	
 
 	public Cake addCake(Cake cake) {
 		return cakeDao.save(cake);
 	}
-	
-	public CakeOccasions[] getAllOccasions(){
+
+	public CakeOccasions[] getAllOccasions() {
 		return CakeOccasions.values();
 	}
-	
+
 	public CakeCategory[] getAllCategories() {
 		return CakeCategory.values();
+	}
+
+	public CakeFlavours[] getAllFlavours() {
+		return CakeFlavours.values();
 	}
 
 	public List<Cake> addCakeList(List<Cake> cakeList) {
@@ -38,34 +41,35 @@ public class CakeService {
 	public List<Cake> getAllCakes() {
 		return cakeDao.findAll();
 	}
-	
+
 	public Cake findCakeById(long id) {
 		return cakeDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cake(id) does not exist"));
 	}
-	
+
 	public List<Cake> findCakeByCategory(CakeCategory category) {
 		return cakeDao.findByCategory(category);
 	}
-	
+
 	public List<Cake> findCakeByOccasions(CakeOccasions occasions) {
 		return cakeDao.findByOccasions(occasions);
 	}
-	
+
 	public List<Cake> findCakeByFlavours(CakeFlavours flavours) {
 		return cakeDao.findByFlavours(flavours);
 	}
-	
+
 	public Cake deleteCakeById(long id) throws Exception {
-		Cake cakeToDelete= findCakeById(id);
-		if(cakeToDelete!=null) {
+		Cake cakeToDelete = findCakeById(id);
+		if (cakeToDelete != null) {
 			cakeDao.deleteById(id);
 			return cakeToDelete;
 		}
 		throw new ResourceNotFoundException("Cake(id) does not exist");
 	}
+
 	public Cake editCakeById(long id, Cake cake) {
-		Cake cakeToEdit= findCakeById(id);
-		if(cakeToEdit!=null) {
+		Cake cakeToEdit = findCakeById(id);
+		if (cakeToEdit != null) {
 			cakeDao.save(cake);
 			return cakeToEdit;
 		}
