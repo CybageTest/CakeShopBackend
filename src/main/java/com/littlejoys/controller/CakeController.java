aa608eb513cd2f7729d54a7090fc6a411c05bf21
Abhijeet <abhijeetgu@cybage.com>
2023-01-02 11:06:44 +0530

    added cross origin

--- src/main/java/com/littlejoys/controller/CakeController.java
+++ src/main/java/com/littlejoys/controller/CakeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.littlejoys.entity.CakeFlavours;
 import com.littlejoys.entity.CakeOccasions;
 import com.littlejoys.service.CakeService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/cake")
 public class CakeController {
