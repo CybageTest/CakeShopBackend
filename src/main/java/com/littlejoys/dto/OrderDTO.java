f7fdda486f79d72be786f651b5f4bc20f59c7d3d
Abhijeet <abhijeetgu@cybage.com>
2023-01-13 14:00:21 +0530

    DTO for order created

---
+++ src/main/java/com/littlejoys/dto/OrderDTO.java
@@ -0,0 +1,119 @@
+package com.littlejoys.dto;
+
+import java.util.Set;
+
+import com.littlejoys.entity.AddOns;
+import com.littlejoys.entity.Cake;
+import com.littlejoys.entity.Offer;
+import com.littlejoys.entity.User;
+
+public class OrderDTO {
+
+	private long id;
+	private String messageOnCake;
+	private Double shippingCost;
+	private Double totalCost;
+	private String address;
+	private Set<Cake> cakes;
+	private User user;
+	private Set<Offer> offer;
+	private Set<AddOns> addons;
+
+	public OrderDTO() {
+
+	}
+
+	public OrderDTO(long id, String messageOnCake, double shippingCost, double totalCost, String address,
+			Set<Cake> cakes, User user, Set<Offer> offer, Set<AddOns> addons) {
+		super();
+		this.id = id;
+		this.messageOnCake = messageOnCake;
+		this.shippingCost = shippingCost;
+		this.totalCost = totalCost;
+		this.address = address;
+		this.cakes = cakes;
+		this.user = user;
+		this.offer = offer;
+		this.addons = addons;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getMessageOnCake() {
+		return messageOnCake;
+	}
+
+	public void setMessageOnCake(String messageOnCake) {
+		this.messageOnCake = messageOnCake;
+	}
+
+	public double getShippingCost() {
+		return shippingCost;
+	}
+
+	public void setShippingCost(double shippingCost) {
+		this.shippingCost = shippingCost;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Set<Cake> getCakes() {
+		return cakes;
+	}
+
+	public void setCakes(Set<Cake> cakes) {
+		this.cakes = cakes;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Set<Offer> getOffer() {
+		return offer;
+	}
+
+	public void setOffer(Set<Offer> offer) {
+		this.offer = offer;
+	}
+
+	public Set<AddOns> getAddons() {
+		return addons;
+	}
+
+	public void setAddons(Set<AddOns> addons) {
+		this.addons = addons;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderDTO [id=" + id + ", messageOnCake=" + messageOnCake + ", shippingCost=" + shippingCost
+				+ ", totalCost=" + totalCost + ", address=" + address + ", cakes=" + cakes + ", user=" + user
+				+ ", offer=" + offer + ", addons=" + addons + "]";
+	}
+
+}
