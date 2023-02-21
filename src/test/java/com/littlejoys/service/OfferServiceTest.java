6898cd9575f5ef52cbd84b242946f34056a843ac
Abhijeet <abhijeetgu@cybage.com>
2023-02-21 17:40:50 +0530

    added test case for addOffer() method

--- src/test/java/com/littlejoys/service/OfferServiceTest.java
+++ src/test/java/com/littlejoys/service/OfferServiceTest.java
@@ -1,16 +1,21 @@
 package com.littlejoys.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
 import com.littlejoys.dao.IOfferDao;
 import com.littlejoys.dto.OfferDTO;
 import com.littlejoys.entity.Offer;
+import com.littlejoys.exception.ResourceAlreadyExistException;
 
 class OfferServiceTest {
 
@@ -28,11 +33,22 @@ class OfferServiceTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+		offerDTO = new OfferDTO(10, "Test DTO offer", "TDO", 10, "Test DTO offer description", null);
+		offer = new Offer(10, "Test DTO offer", "TDO", 10, "Test DTO offer description", null);
 	}
 
 	@Test
 	void testAddOffer() {
-		fail("Not yet implemented");
+		when(modelMapper.map(any(OfferDTO.class), any())).thenReturn(offer);
+		when(offerDao.save(any(Offer.class))).thenReturn(offer);
+		Offer result = null;
+		try {
+			result = offerService.addOffer(offerDTO);
+		} catch (ResourceAlreadyExistException e) {
+			e.printStackTrace();
+		}
+		assertEquals(offer, result);
 	}
 
 	@Test
