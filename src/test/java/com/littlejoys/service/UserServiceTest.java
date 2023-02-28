0b4b950190e9f845786c1be724de1a7c76b75e69
Abhijeet <abhijeetgu@cybage.com>
2023-02-28 18:21:22 +0530

    modified setUp() method

--- src/test/java/com/littlejoys/service/UserServiceTest.java
+++ src/test/java/com/littlejoys/service/UserServiceTest.java
@@ -1,24 +1,24 @@
 package com.littlejoys.service;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.modelmapper.ModelMapper;
 
 import com.littlejoys.dao.IRoleDao;
 import com.littlejoys.dao.IUserDao;
-import com.littlejoys.demo.CakeShopApplication;
+import com.littlejoys.dto.UserDTO;
 import com.littlejoys.entity.Role;
 import com.littlejoys.entity.User;
 
-
 class UserServiceTest {
+
+	@Mock
+	private ModelMapper modelMapper;
 
 	@InjectMocks
 	private UserService userService;
@@ -29,18 +29,22 @@ class UserServiceTest {
 	@Mock
 	private IRoleDao roleDao;
 
+	private User user;
+	private UserDTO userDTO;
+
 	@BeforeEach
-	void setup() {
+	void setUp() throws Exception {
 		Role userRole = new Role("user", "default role for user");
 		Set<Role> role = new HashSet<>();
 		role.add(userRole);
-		Optional<User> user1 = Optional
-				.of(new User("abc", "abc@mail.com", "abhi@123", "9850909090", "inactive", null, role, null));
+		user = new User("testUser", "test@mail.com", "test@123", "9850909090", "inactive", null, role, null);
+		userDTO = new UserDTO("testUserDto", "testDto@mail.com", "testDto@123", "9850909090", "inactive", null, role,
+				null);
 	}
 
 	@Test
 	void getUserById_Sucess() {
-		
+
 	}
 
 //	@Test
